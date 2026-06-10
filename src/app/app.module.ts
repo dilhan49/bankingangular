b413d19d78c21d74b8721c67d3a92d110ebf03c1
Dilhan BAS <d.bas@nibelis.com>
2026-06-10 20:31:12 +0200

    interface html ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { RegisterComponent } from './pages/register/register.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
 import { LightInfoComponent } from './components/light-info/light-info.component';
+import { MyTransactionsComponent } from './pages/my-transactions/my-transactions.component';
+import { MyContactListComponent } from './pages/my-contact-list/my-contact-list.component';
+import { NewTransactionComponent } from './pages/new-transaction/new-transaction.component';
+import { NewContactComponent } from './pages/new-contact/new-contact.component';
+import { MyProfileComponent } from './pages/my-profile/my-profile.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { LightInfoComponent } from './components/light-info/light-info.component
     RegisterComponent,
     MenuComponent,
     UserDashboardComponent,
-    LightInfoComponent
+    LightInfoComponent,
+    MyTransactionsComponent,
+    MyContactListComponent,
+    NewTransactionComponent,
+    NewContactComponent,
+    MyProfileComponent
   ],
   imports: [
     BrowserModule,
