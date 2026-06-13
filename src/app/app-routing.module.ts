eaf4b69c594d3542a3cd58296aa34b92ad83ef72
Dilhan BAS <d.bas@nibelis.com>
2026-06-13 15:27:55 +0200

    Routage de la solution web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
+import { MyTransactionsComponent } from './pages/my-transactions/my-transactions.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { MyContactListComponent } from './pages/my-contact-list/my-contact-list.component';
+import { NewTransactionComponent } from './pages/new-transaction/new-transaction.component';
+import { NewContactComponent } from './pages/new-contact/new-contact.component';
+import { MyProfileComponent } from './pages/my-profile/my-profile.component';
+import { MainAdminPageComponent } from './pages/main-admin-page/main-admin-page.component';
+import { ManageUsersComponent } from './admin/manage-users/manage-users.component';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   {
@@ -10,14 +20,73 @@ const routes: Routes = [
 
   },
   {
-    path: 'login/:someText',
-    component: LoginComponent
-
-  },
-  {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'user',
+    component: MainPageComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: UserDashboardComponent
+      },
+      {
+        path: 'my-transactions',
+        component: MyTransactionsComponent
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'my-contact-list',
+        component: MyContactListComponent
+      },
+      {
+        path: 'new-transaction',
+        component: NewTransactionComponent
+      },
+      {
+        path: 'new-contact/:idContact',
+        component: NewContactComponent
+      },
+      {
+        path: 'new-contact',
+        component: NewContactComponent
+      },
+      {
+        path: 'profile',
+        component: MyProfileComponent
+      }
+    ]
+  },
+  {
+    path: 'admin',
+    component: MainAdminPageComponent,
+    children: [
+      {
+        path: 'customers',
+        component: ManageUsersComponent
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: AdminDashboardComponent
+      },
+      {
+        path: 'profile',
+        component: MyProfileComponent
+      }
+    ]
   }
+
+
 ];
 
 @NgModule({
