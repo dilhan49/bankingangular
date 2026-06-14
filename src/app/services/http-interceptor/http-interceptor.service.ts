078964d217532b49465b2a125f13eca09a0d5cec
Dilhan BAS <d.bas@nibelis.com>
2026-06-14 17:03:50 +0200

    Correction http-interceptor

--- src/app/services/http-interceptor/http-interceptor.service.ts
+++ src/app/services/http-interceptor/http-interceptor.service.ts
@@ -9,16 +9,19 @@ export class HttpInterceptorService implements HttpInterceptor{
 
   constructor() { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJiYXMuZGlsaGFuNDlAZ21haWwuY29tIiwiZnVsbE5hbWUiOiJEaWxoYW4gQkFTIiwiZXhwIjoxNzgyMDc5NzcyLCJ1c2VySWQiOjIsImlhdCI6MTc4MTM1OTc3MiwiYXV0aG9yaXRpZXMiOlt7ImF1dGhvcml0eSI6IlJPTEVfVVNFUiJ9XX0.KvIheQ7LZmCkJjzfzKTqzNlUYGCfjfEm8r4MdTiEfvQ';
-    if(token !== undefined && token !== null){
-      //assigner le token;
-      const authReq = req.clone({
-        headers: new HttpHeaders({
-          Authorization : 'Bearer ' + token
-        })
-      });
-      return next.handle(authReq);
+    if(!req.url.includes('/authenticate') || !req.url.includes('/register')) {
+      const token = localStorage.getItem('token');
+      if(token){
+        //assigner le token;
+        const authReq = req.clone({
+          headers: new HttpHeaders({
+            Authorization : 'Bearer ' + token
+          })
+        });
+        return next.handle(authReq);
+      }
     }
+
     return next.handle(req);
   }
 }
